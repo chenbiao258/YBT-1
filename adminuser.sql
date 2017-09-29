93281a4e562efa977765f3f8992516d2b45b9e6f
suojinyu <jinyuhehea@126.com>
2017-09-29 17:09:49 +0800

    Signed-off-by: suojinyu <jinyuhehea@126.com>

---
+++ adminuser.sql
@@ -0,0 +1,7 @@
+﻿prompt Importing table lduser...
+set feedback off
+set define off
+insert into lduser (EMPLID, DEPARTMENT, NAME, JOBCODE, COMPANYCODE, COMPANYNAME, LOCATION, STATUS, HIREDATE, REHIREDATE, CHECKFLAG, OPERATOR, MAKEDATE, MAKETIME)
+values ('admin', null, 'admin', null, '001', null, null, 'active', null, null, null, null, null, null);
+
+prompt Done.
