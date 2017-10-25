a1679cb334f69ec19e0c2451f973790e12e69042
suojinyu <jinyuhehea@126.com>
2017-10-25 21:16:39 +0800

    Signed-off-by: suojinyu <jinyuhehea@126.com>

---
+++ menu1.sql
@@ -0,0 +1,10 @@
+prompt Importing table menu...
+set feedback off
+set define off
+insert into menu (ID, PAR_UID, MENU_NAME, MENU_DESC, MENU_STATUS, MENU_LINK, MENU_INDEX, T_S_UID, MEDDLE_MENU, MEDDLE_MENU_NAME, CRT_TIME, UPD_TIME, SUB_MENU_NAME, SUB_MENU)
+values ('49b9ad-15f2986c497-5deb523e948420889a2a376f1a539c8e', null, '内部培训新增', '10', null, '/application/customermanager/newCustomermanager/jsp/othertrainingAdd.jsp', 0, null, 3, 'RM资质管理模块', to_date('24-10-2017', 'dd-mm-yyyy'), to_date('24-10-2017', 'dd-mm-yyyy'), '人员管理', 2);
+
+insert into menu (ID, PAR_UID, MENU_NAME, MENU_DESC, MENU_STATUS, MENU_LINK, MENU_INDEX, T_S_UID, MEDDLE_MENU, MEDDLE_MENU_NAME, CRT_TIME, UPD_TIME, SUB_MENU_NAME, SUB_MENU)
+values ('49b9ad-15f2986c497-5deb523e948420889a8a376f1a539c8e', null, '内部培训上传', '9', null, '/application/customermanager/newCustomermanager/jsp/othertraining.jsp', 0, null, 3, 'RM资质管理模块', to_date('24-10-2017', 'dd-mm-yyyy'), to_date('24-10-2017', 'dd-mm-yyyy'), '人员管理', 2);
+
+prompt Done.
---
+++ menu2.sql
@@ -0,0 +1,7 @@
+prompt Importing table menu...
+set feedback off
+set define off
+insert into menu (ID, PAR_UID, MENU_NAME, MENU_DESC, MENU_STATUS, MENU_LINK, MENU_INDEX, T_S_UID, MEDDLE_MENU, MEDDLE_MENU_NAME, CRT_TIME, UPD_TIME, SUB_MENU_NAME, SUB_MENU)
+values ('3386c44-15d54d573f5-49213c8f0e8758438d7a34e7889a8b34', null, '内部培训维护', '5', null, '/application/aptitude/newAptitude/jsp/addOther.jsp', 0, null, 4, '资质数据维护', to_date('25-10-2017', 'dd-mm-yyyy'), to_date('25-10-2017', 'dd-mm-yyyy'), '人员管理', 2);
+
+prompt Done.
