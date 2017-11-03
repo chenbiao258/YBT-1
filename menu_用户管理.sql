e74b8a90cb44aecbb44805c85e7effa47c32d9a6
suojinyu <jinyuhehea@126.com>
2017-11-03 11:22:34 +0800

    Signed-off-by: suojinyu <jinyuhehea@126.com>

--- menu_用户管理.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table menu...
-set feedback off
-set define off
-insert into menu (ID, PAR_UID, MENU_NAME, MENU_DESC, MENU_STATUS, MENU_LINK, MENU_INDEX, T_S_UID, MEDDLE_MENU, MEDDLE_MENU_NAME, CRT_TIME, UPD_TIME, SUB_MENU_NAME, SUB_MENU)
-values ('4986c47-15d46d573f5-67213c8f0e8758438d7a34e7819a8b34', null, '用户查询', '3', null, '/application/newSystemManage/userManage/jsp/UserAddInputCheck.jsp', 0, null, 2, '用户管理', to_date('21-09-2017', 'dd-mm-yyyy'), to_date('14-09-2017', 'dd-mm-yyyy'), '人员管理', 2);
-
-prompt Done.
